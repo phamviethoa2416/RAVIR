95f253a748f3d342ddf8070d856843424c1fc61b
phamhoa2416 <phamviethoa2416@gmail.com>
2026-03-05 14:47:28 +0700

    feat: add multi-head losses

--- losses/__init__.py
+++ losses/__init__.py
@@ -1,3 +1,23 @@
-from .losses import DiceLoss, CombinedLoss
+from .losses import (
+    DiceLoss,
+    CombinedLoss,
+    TverskyLoss,
+    TverskyCELoss,
+    VesselProbLoss,
+    OrientationLoss,
+    WidthMapLoss,
+    EndpointLoss,
+    MultiHeadLoss,
+)
 
-__all__ = ["DiceLoss", "CombinedLoss"]
+__all__ = [
+    "DiceLoss",
+    "CombinedLoss",
+    "TverskyLoss",
+    "TverskyCELoss",
+    "VesselProbLoss",
+    "OrientationLoss",
+    "WidthMapLoss",
+    "EndpointLoss",
+    "MultiHeadLoss",
+]
